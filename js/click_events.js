61bb1c6a1bc8514a29125cc77f11527afc89bbad
Fjayala333 <83990325+Fjayala333@users.noreply.github.com>
2021-09-14 15:22:10 -0700

    display different data

---
+++ js/click_events.js
@@ -0,0 +1,31 @@
+trafficBar = document.querySelectorAll(".traffic-nav-link");
+const updateChart = (chart, newData) => {
+    chart.data.labels = newData.labels;
+    chart.data.datasets[0].data = newData.datasets[0].data;
+    chart.update({
+      duration: 800,
+      easing: 'linear',
+    });
+  };
+
+trafficBar.forEach(navItem => {
+    navItem.addEventListener("click", function(){
+        trafficBar.forEach(nav => nav.classList.remove('active'));
+        this.classList.add('active');
+        if (navItem.textContent === "Hourly" ){
+            updateChart(trafficChart, trafficDataHourly);
+            
+        }
+        else if (navItem.textContent === "Daily" ){
+            updateChart(trafficChart, trafficDataDaily);
+            
+        }
+        else if (navItem.textContent === "Weekly" ){
+            updateChart(trafficChart, trafficDataWeekly);
+            
+        }
+        else if (navItem.textContent === "Monthly" ){
+            updateChart(trafficChart, trafficDataMonthly);
+        }
+    });
+});
